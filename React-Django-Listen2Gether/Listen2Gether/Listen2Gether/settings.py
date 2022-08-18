e95c1e21eabecdebe69ebe32a1983c7a493e353d
a3eon <35718739+a3eon-1@users.noreply.github.com>
2022-08-17 23:18:36 -0700

    Update settings.py

--- React-Django-Listen2Gether/Listen2Gether/Listen2Gether/settings.py
+++ React-Django-Listen2Gether/Listen2Gether/Listen2Gether/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v@qjew58^s3yk@86&-9qs(wr3nke068h&t99^%9iiok_pe0v48'
+SECRET_KEY = ########
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
